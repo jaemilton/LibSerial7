0806f1e52a29964c0d9b3bdf208856a98620a36f
Jaemilton Ribeiro de Oliveira <jaemiltonro@yahoo.com.br>
2019-08-17 15:25:33 -0300

    Ajustes de config

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Jaemilton',                  
   author_email = 'jaemilton@gmail.com',     
   url = 'https://github.com/jaemilton/LibSerial7',  
-  download_url = 'https://github.com/microgenios/LibModbus/archive/0.1.tar.gz',    
+  download_url = 'https://github.com/jaemilton/LibSerial7/archive/0.1.tar.gz',    
   keywords = ['Serial', 'Senai', 'ComPort'],  
   install_requires=[           
           'datetime',
